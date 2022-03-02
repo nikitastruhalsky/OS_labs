53253d5e92b2ddac5f7d83bedcde9a329cee9a1d
Nikita <strugalsky03@gmail.com>
2022-03-02 23:51:20 +0300

    lab1 (linux) done

---
+++ number1/library.cpp
@@ -0,0 +1,43 @@
+#include "library.h"
+
+namespace NumberStaticLibrary
+        {
+                Number operator+(Number a, Number b)
+        {
+            return a.number + b.number;
+        }
+        Number Number::Add(Number a, Number b)
+        {
+            return a + b;
+        }
+
+
+        Number operator-(Number a, Number b)
+        {
+            return a.number - b.number;
+        }
+        Number Number::Subtract(Number a, Number b)
+        {
+            return a - b;
+        }
+
+
+        Number operator*(Number a, Number b)
+        {
+            return a.number * b.number;
+        }
+        Number Number::Multiply(Number a, Number b)
+        {
+            return a * b;
+        }
+
+
+        Number operator/(Number a, Number b)
+        {
+            return a.number / b.number;
+        }
+        Number Number::Divide(Number a, Number b)
+        {
+            return a / b;
+        }
+        }
